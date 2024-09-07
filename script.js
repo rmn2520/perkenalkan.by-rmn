1e18fee9d9b40142422243f5951154c208a9eafe
rmn2520 <ramonuhuyy@gmail.com>
2024-09-07 09:52:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Pesan di console saat halaman dimuat
+console.log("Halaman perkenalan berhasil dimuat!");
+
+// Fungsi animasi tambahan jika dibutuhkan di masa depan
+function showWelcomeMessage() {
+    alert("Selamat datang di halaman saya!");
+}
